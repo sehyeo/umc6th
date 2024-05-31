08a70bf1f89e0c108d1e721beb268a896d44f244
김승현 <145731052+sehyeo@users.noreply.github.com>
2024-05-31 09:51:11 +0900

    Add files via upload

---
+++ umc/spring/study/domain/Review.java
@@ -0,0 +1,33 @@
+package umc.spring.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.study.domain.common.BaseEntity;
+import umc.spring.study.domain.mapping.MemberAgree;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String title;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
